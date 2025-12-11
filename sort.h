53387ec3f5f58736af30190b2a145c9791b2bf6d
ylabate <12196@holbertonstudents.com>
2025-12-11 08:46:22 +0000

    Format quick_sort_child function declaration for improved readability

--- sort.h
+++ sort.h
@@ -27,6 +27,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_child(int *array, size_t size, int **first_element, int length_total);
+void quick_sort_child(int *array, size_t size, int **first_element,
+	int length_total);
 
 #endif /* __SORT_H__ */
